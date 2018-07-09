8f63927e08d141f43a337785a5177e5be2cadffc
David Tomaschik <david@systemoverlord.com>
2018-07-08 18:47:13 -0700

    Remove unused menu.

--- android/app/src/main/java/com/attackercommunity/acdcbadge/HomeActivity.java
+++ android/app/src/main/java/com/attackercommunity/acdcbadge/HomeActivity.java
@@ -86,21 +86,6 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_ENABLE_BT && resultCode == RESULT_OK) {
             mBadgeListAdapter.refreshView(this);
